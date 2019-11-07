7f0873d00971b9f3ea726833e19b93e132f1745c
张三 <2577837757@example.com>
2019-11-07 16:18:02 +0800

    111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111

--- main.go
+++ main.go
@@ -10,5 +10,11 @@ func main()  {
 	fmt.Println("Add(10,20):",res)
 	res=calc.Sub(30,20)
 	fmt.Println("Sub(30,20)",res)
+	res=calc.Multi(10,20)
+
+	fmt.Println("Multi(10,20)",res)
+	res=calc.Div(30,10)
+
+	fmt.Println("Div(30,10)",res)
 
 }
